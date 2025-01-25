c17d17b7693e916e0cea67cacbdd58cc6a81e7ae
windows 10 <windows 10.gmail.com>
2025-01-25 21:56:40 +0530

    more todo stuff

--- todo/src/main/java/com/balyan/todo/Services/TaskService.java
+++ todo/src/main/java/com/balyan/todo/Services/TaskService.java
@@ -2,8 +2,12 @@ package com.balyan.todo.Services;
 
 import com.balyan.todo.Repo.TasksRepo;
 import com.balyan.todo.entity.Task;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
 
 @Component
 public class TaskService {
@@ -11,5 +15,11 @@ public class TaskService {
     TasksRepo taskrepo;
     public void savetask(Task naya){
         taskrepo.save(naya);
+    }
+    public List<Task> getall(){
+        return taskrepo.findAll();
+    }
+    public Optional<Task> getbyid(ObjectId mid){
+        return taskrepo.findById(mid);
     }
 }
--- todo/src/main/java/com/balyan/todo/controller/taskController.java
+++ todo/src/main/java/com/balyan/todo/controller/taskController.java
@@ -3,6 +3,8 @@ package com.balyan.todo.controller;
 import com.balyan.todo.Repo.TasksRepo;
 import com.balyan.todo.Services.TaskService;
 import com.balyan.todo.entity.Task;
+import lombok.NonNull;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,14 @@ public class taskController {
     @PostMapping()
     public void createtask(@RequestBody Task aya){
        taskService.savetask(aya);
+    }
+    @GetMapping()
+    public List<Task> getall(){
+        return taskService.getall();
+    }
+    @GetMapping("/id/{mid}")
+    public Task getbyid(@PathVariable ObjectId mid){
+        return taskService.getbyid(mid).orElse(null);
     }
 
 
